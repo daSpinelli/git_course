ff2004ce07bec488057f6648efab9991d05d918c
daspinelli <denny.dealmeida@gmail.com>
2023-01-05 16:16:17 -0300

    Added main function

--- app.py
+++ app.py
@@ -5,8 +5,10 @@ import streamlit as st
 def load_data(_file):
     return pd.read_csv(_file)
 
-file = 'data/processed/bikes_completed.csv'
+def main():
+    file = 'data/processed/bikes_completed.csv'
+    df = load_data(file)
+    st.dataframe(df)
 
-df = load_data(file)
-
-st.dataframe(df)
+if __name__ == '__main__':
+    main()

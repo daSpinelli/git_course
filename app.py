48c76fb2c914694a7ecde98c1f4e99df4cfac08f
daspinelli <denny.dealmeida@gmail.com>
2023-01-05 16:14:43 -0300

    Created app.py file to load and present data

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def load_data(_file):
+    return pd.read_csv(_file)
+
+file = 'data/processed/bikes_completed.csv'
+
+df = load_data(file)
+
+st.dataframe(df)
